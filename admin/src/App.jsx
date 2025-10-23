aa36b10e43fc561680796c7b04db684b6e48eec0
Dulaj Gayantha <dulaj.gayantha911@gmail.com>
2025-10-23 12:24:49 +0530

    initilized the basics of admin panel, created the form for login for a admin/doctor

---
+++ admin/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+ import { ToastContainer, toast } from 'react-toastify';
+ import { AdminContext } from './context/AdminContext';
+
+const App = () => {
+  const {aToken} = useContext(AdminContext)
+  return aToken?(
+    <div >
+      
+      <ToastContainer />
+    </div>
+  ):(
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
