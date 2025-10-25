dedb33d75a1f29a81894557bf0e006d698f6c7f5
Dulaj Gayantha <dulaj.gayantha911@gmail.com>
2025-10-25 09:59:23 +0530

    added a new logo and taglines

--- frontend/src/components/SpecialityMenue.jsx
+++ frontend/src/components/SpecialityMenue.jsx
@@ -5,7 +5,7 @@ const SpecialityMenue = () => {
   return (
     <div id='speciality' className='flex flex-col items-center gap-4 py-16 text-gray-800'>
         <h1 className='text-3xl font-medium'>Find by speciality</h1>
-        <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+        <p className='sm:w-1/3 text-center text-sm'>Trusted care is just a few clicks away. Choose your specialist and schedule today.</p>
 
         <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
             {specialityData.map((item,index)=>(
