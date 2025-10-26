9aa6f3f7eff3ad2eccb676184e55d6652649e6a4
Dulaj Gayantha <dulaj.gayantha911@gmail.com>
2025-10-26 16:07:51 +0530

    ui-color changes

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -16,8 +16,8 @@ const TopDoctors = () => {
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0,10).map((item,index)=>(
-                <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
-                    <img className='bg-blue-50' src={item.image} />
+                <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}} key={index} className='border border-gray-300 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                    <img className='bg-indigo-50' src={item.image} />
                     <div className='p-4'>
                         <div className='flex ite gap-2 text-sm text-center text-green-500'>
                             <p className='w-2 h-2 bg-green-500 rounded-full'></p>
